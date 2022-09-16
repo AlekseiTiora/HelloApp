f5860857dcaf68fc1f0f7b39c70b9e25a1bdf4a6
opilane <opilane@HP-CZC6297B8F>
2022-09-16 11:37:21 +0300

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using HelloApp;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    // создаем два объекта User
+    User tom = new User { Name = "Nikita", Age = 33, Gender = "Male" };
+    User alice = new User { Name = "Olesja", Age = 26, Gender ="female" };
+
+    // добавляем их в бд
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    db.SaveChanges();
+    Console.WriteLine("Objektide salvestamine õnnestus");
+
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Objektide loend:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age} - {u.Gender}");
+    }
+}
